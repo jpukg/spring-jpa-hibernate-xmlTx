0744b72b89c39fad5e9996bc6beff33f39c6b1c5
luoyong <570911467@qq.com>
2014-08-19 12:08:34 +0800

    first commit code

---
+++ src/main/java/name/luoyong/spring/jpa/service/StoneService.java
@@ -0,0 +1,24 @@
+package name.luoyong.spring.jpa.service;
+
+import name.luoyong.spring.jpa.dao.StoneDao;
+import name.luoyong.spring.jpa.entity.Stone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StoneService {
+	
+	@Autowired
+	private StoneDao stoneDao;
+	
+	public void save(Stone stone) {
+		stoneDao.save(stone);
+	}
+	
+	public void delete(Long id) {
+		stoneDao.delete(id);
+	}
+
+}
